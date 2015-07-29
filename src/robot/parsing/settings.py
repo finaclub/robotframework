318b51ee795b6115c7eeb56bf8664fe36bc89cfe
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-07-29 23:44:44 +0200

    [python3] parsing.settings: fixed accidentally removed six.PY3 import

--- src/robot/parsing/settings.py
+++ src/robot/parsing/settings.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from six import PY3
+
 from robot.utils import is_string
 
 from .comments import Comment
